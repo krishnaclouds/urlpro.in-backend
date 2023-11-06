4c39ce930bb4e0b933ab5e8041f42dc44d5cb14b
Bala Krishna <prince.balakrishna@gmail.com>
2023-11-06 14:20:01 +0530

    Updated ReadMe

--- initializers/routes/routes.go
+++ initializers/routes/routes.go
@@ -5,6 +5,7 @@ import (
 	c "urlpro/controllers/url"
 )
 
+// InitRoutes initializes the application routes and starts the server.
 func InitRoutes() {
 
 	// Get the url port number from the configuration function
@@ -20,15 +21,15 @@ func InitRoutes() {
 	router.POST("/generate", c.GenerateShortURL)
 	router.GET("/:shortURL", c.RedirectToDesiredURL)
 
-	// Start the Gin server on the specified port
+	// Start the Gin server on the specified host and port
 	if err := router.Run(hostURL); err != nil {
-		// Handle any errors that occur while starting the server
 		panic(err)
 	}
-
 }
 
-// getConfig returns url of the application.
+// getConfig returns the host and port configuration.
 func getConfig() (string, error) {
+	// You can load the configuration from a file, environment variable, or any other source.
+	// Here, we're using a hard-coded value for demonstration purposes.
 	return ":8084", nil
 }
